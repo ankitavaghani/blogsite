4904d91a7c93a9f0d0c484f3fbc3eb21a77c1bd3
Ankita Vaghani <ankitamaster1+pp@gmail.com>
2019-07-23 23:37:01 +0530

    login authentication required in add/delete comment

--- blog/views.py
+++ blog/views.py
@@ -65,6 +65,7 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -79,6 +80,7 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
         return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
+@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
